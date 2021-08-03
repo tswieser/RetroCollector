751643f5c585d3edb5da563c501135d605c13bce
Timothy Wieser <timothyswieser@gmail.com>
2021-08-03 15:03:17 -0400

    games styled

--- react-app/src/components/createGameModal/CreateGame.js
+++ react-app/src/components/createGameModal/CreateGame.js
@@ -21,7 +21,7 @@ function CreateGame({ setShowModal, consoleInfo }) {
     const games = useSelector(state => Object.values(state.games))
     const addDecimal = (num) => (num / 100).toFixed(2)
 
-  
+
     useEffect(() => {
         if (games.length) {
             if (condition === "CIB" && games[games.length - 1]["console-name"]) {
@@ -72,39 +72,41 @@ function CreateGame({ setShowModal, consoleInfo }) {
 
 
     return (
-        <div>
+        <div className="game_container_modal">
+            <div className="title_container">
+                <h1 className='edit_game_form_title'>Add A Game</h1>
+            </div>
+            <ul className="form_error_container">
+                {errors &&
+                    errors.map((error, i) => (
+                        <li className="errors" key={i}>
+                            {error}
+                        </li>
+                    ))}
+            </ul>
             <div className="search_container">
                 <form onSubmit={onSearch}>
                     <input
-                        placeholder='Search item'
+                        placeholder='Quick Search'
                         onKeyUp={(e) => setGameSearch(e.target.value)}
+                        className="search_field"
                     >
                     </input>
-                    <button>
-                        Search
+                    <button className="search_button">
+                        <i class="fas fa-search"></i>
                     </button>
                 </form>
             </div>
-            <div className="form_container">
+            <div className="game_form_container">
                 <form onSubmit={handleSubmit}>
-                    <div className="title_container">
-                        <h1 className='form_title'>Add A Game</h1>
-                    </div>
-                    <ul className="form_error_container">
-                        {errors &&
-                            errors.map((error, i) => (
-                                <li className="errors" key={i}>
-                                    {error}
-                                </li>
-                            ))}
-                    </ul>
+
                     <div className="form_label_container">
                         <label htmlFor="name" className="form_labels">
                             Title
                         </label>
                     </div>
                     <div>
-                        <input id="name" className="collection_name" name="name" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        <input id="name" className="game_input_name" name="name" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
                     </div>
                     <div className="form_label_container">
                         <label htmlFor="name" className="form_labels">
@@ -112,17 +114,21 @@ function CreateGame({ setShowModal, consoleInfo }) {
                         </label>
                     </div>
                     <div>
-                        <input id="genre" className="collection_imgUrl" name="genre" type="text" value={genre} onChange={(e) => setGenre(e.target.value)} />
+                        <input id="genre" className="game_input_name" name="genre" type="text" value={genre} onChange={(e) => setGenre(e.target.value)} />
                     </div>
                     <div className="radio_buttons">
-                        Loose<input type="radio" value="loose" checked={condition === "loose"}
-                            onChange={(e) => { setCondition(e.target.value) }}
-                        >
-                        </input>
-                        CIB<input type="radio" value="CIB" checked={condition === "CIB"}
-                            onChange={(e) => { setCondition(e.target.value) }}
-                        >
-                        </input>
+                        <div>
+                            Loose<input type="checkbox" value="loose" checked={condition === "loose"}
+                                onChange={(e) => { setCondition(e.target.value) }}
+                            >
+                            </input>
+                        </div>
+                        <div className="cib_button">
+                            CIB<input className="checkbox_button" type="checkbox" value="CIB" checked={condition === "CIB"}
+                                onChange={(e) => { setCondition(e.target.value) }}
+                            >
+                            </input>
+                        </div>
 
                     </div>
                     <div className="form_label_container">
@@ -131,7 +137,7 @@ function CreateGame({ setShowModal, consoleInfo }) {
                         </label>
                     </div>
                     <div>
-                        <input id="value" className="collection_description" name="value" type="number" value={`${value}`} onChange={(e) => setValue(e.target.value)} />
+                        <input id="value" className="game_input_name" name="value" type="number" value={`${value}`} onChange={(e) => setValue(e.target.value)} />
                     </div>
                     <div className="form_label_container">
                         <label htmlFor="name" className="form_labels">
@@ -139,10 +145,10 @@ function CreateGame({ setShowModal, consoleInfo }) {
                         </label>
                     </div>
                     <div>
-                        <input placeholder="Optional" id="release_date" className="collection_description" name="release_date" type="text" value={release_date} onChange={(e) => setReleaseDate(e.target.value)} />
+                        <input placeholder="Optional" id="release_date" className="game_input_name" name="release_date" type="text" value={release_date} onChange={(e) => setReleaseDate(e.target.value)} />
                     </div>
                     <div className="button_container">
-                        <button className="form_button" type="submit">
+                        <button className="game_form_button" type="submit">
                             Add Game
                         </button>
                     </div>
--- react-app/src/components/createGameModal/index.js
+++ react-app/src/components/createGameModal/index.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import CreateGame from "./CreateGame";
-
+import "./createGame.css"
 
 function GameModal({ consoleInfo }) {
     const [showModal, setShowModal] = useState(false);
-    
+
     return (
         <>
-            <div onClick={() => setShowModal(true)}>
-                <i class="fas fa-plus fa-2x"></i>
-            </div>
-            {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <CreateGame setShowModal={setShowModal} consoleInfo={consoleInfo} />
-                </Modal>
-            )}
+            <div className="add_game_container" onClick={() => setShowModal(true)}>
+                <div className="plus_button">
+                    <i class="fas fa-plus fa-2x"></i>
+                </div>
+                <div className="add_game">
+                    Add Game to {consoleInfo.name}
+                </div>
+            </div >
+            {
+                showModal && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <CreateGame setShowModal={setShowModal} consoleInfo={consoleInfo} />
+                    </Modal>
+                )
+            }
         </>
     );
 }
